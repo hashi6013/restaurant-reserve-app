801f90b5dfc34f0de78fe0f50f510e85f94d9db8
hashimoto takumi <takumih6013@gmail.com>
2024-04-08 07:02:18 +0900

    コントローラ編集

--- src/app/Http/Controllers/RestaurantController.php
+++ src/app/Http/Controllers/RestaurantController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Restaurant;
+use App\Models\Genre;
 
 class RestaurantController extends Controller
 {
     public function index()
     {
-        return view('index');
+        $restaurants = Restaurant::with('genre')->paninate(20);
+        $genres = Restaurant::all();
+        return view('index', compact('restaurants', genres));
     }
     public function thanks()
     {
